7f40349a687f4f6bb3db0bffc30b9f44872f56b3
Emimint <90863470+Emimint@users.noreply.github.com>
2023-03-11 07:57:59 -0500

    set new custom model in settings.py
    We made custom userprofile in models.py and are now setting them in the Django settings file.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
+# We specified the app where to look for, then the name of the class with the custom user profile.
